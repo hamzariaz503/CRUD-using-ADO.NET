5cd77663f0a7da443606a3cabdf3fa42bb5019a8
hamzariaz503 <riazhamza874@gmail.com>
2024-03-16 17:42:00 +0500

    Add project files.

---
+++ CRUD/Controllers/HomeController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Web.Mvc;
+using CRUD.Models;
+
+namespace CRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            try
+            {
+                EmployeeDbContext db = new EmployeeDbContext();
+                List<Employee> obj = db.GetEmployees();
+                return View(obj);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error occurred while retrieving employees: " + ex.Message);
+                
+                return View("Error");
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    EmployeeDbContext context = new EmployeeDbContext();
+
+                    bool check = context.AddEmployee(emp);
+                    if (check)
+                    {
+                        TempData["InsertMessage"] = "Data inserted successfully";
+                        ModelState.Clear();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Failed to insert data");
+                    }
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine("Error occurred while adding employee: " + ex.Message);
+               
+                return View("Error");
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            EmployeeDbContext context = new EmployeeDbContext();
+            var row = context.GetEmployees().Find(model => model.id == id);
+            return View(row);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id,Employee emp)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    EmployeeDbContext context = new EmployeeDbContext();
+
+                    bool check = context.UpdateEmployee(emp);
+                    if (check)
+                    {
+                        TempData["UpdateMessage"] = "Data Upadte successfully";
+                        ModelState.Clear();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Failed to update data");
+                    }
+                }
+             
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error occurred while updating employee: " + ex.Message);
+
+                return View("Error");
+            }
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            EmployeeDbContext context = new EmployeeDbContext();
+            var row = context.GetEmployees().Find(model => model.id == id);
+            return View(row);
+        }
+        public ActionResult Delete(int id)
+        {
+            EmployeeDbContext context = new EmployeeDbContext();
+            var row = context.GetEmployees().Find(model => model.id == id);
+            return View(row);
+        }
+        [HttpPost]
+        public ActionResult Delete(Employee emp,int id)
+        {
+            EmployeeDbContext context = new EmployeeDbContext();
+
+            bool check = context.DeleteEmployee(id);
+            if (check)
+            {
+                TempData["DeleteMessage"] = "Data Delete successfully";
+                ModelState.Clear();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Failed to delete data");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ CRUD/Models/EmployeeDbContext.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CRUD.Models
+{
+    public class EmployeeDbContext
+    {
+        string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> EmployeesList = new List<Employee>();
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand("spGetEmployees1", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        Employee emp = new Employee();
+                        emp.id = Convert.ToInt32(dr["id"]);
+                        emp.name = dr["name"].ToString();
+                        emp.gender = dr["gender"].ToString();
+                        emp.age = Convert.ToInt32(dr["age"]);
+                        emp.salary = Convert.ToInt32(dr["salary"]);
+                        emp.city = dr["city"].ToString();
+                        EmployeesList.Add(emp);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine("Error occurred while retrieving employees: " + ex.Message);
+            }
+
+            return EmployeesList;
+        }
+
+        public bool AddEmployee(Employee emp)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand("spAddEmployee", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@name", emp.name);
+                    cmd.Parameters.AddWithValue("@gender", emp.gender);
+                    cmd.Parameters.AddWithValue("@age", emp.age);
+                    cmd.Parameters.AddWithValue("@salary", emp.salary);
+                    cmd.Parameters.AddWithValue("@city", emp.city);
+                    con.Open();
+                    int i = cmd.ExecuteNonQuery();
+                    return i > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine("Error occurred while adding employee: " + ex.Message);
+                throw new Exception("An error occurred while adding an employee. Please try again later.", ex);
+            }
+        }
+
+
+        public bool UpdateEmployee(Employee emp)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand("spUpdateEmployee", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@id", emp.id);
+                    cmd.Parameters.AddWithValue("@name", emp.name);
+                    cmd.Parameters.AddWithValue("@gender", emp.gender);
+                    cmd.Parameters.AddWithValue("@age", emp.age);
+                    cmd.Parameters.AddWithValue("@salary", emp.salary);
+                    cmd.Parameters.AddWithValue("@city", emp.city);
+                    con.Open();
+                    int i = cmd.ExecuteNonQuery();
+                    return i > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error occurred while adding employee: " + ex.Message);
+                throw new Exception("An error occurred while adding an employee. Please try again later.", ex);
+            }
+        }
+
+        public bool DeleteEmployee(int id)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand("spDeleteEmployee", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@id", id);
+                    con.Open();
+                    int i = cmd.ExecuteNonQuery();
+                    return i > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while deleting employee: " + ex.Message);
+                throw new Exception("An error occurred while deleting an employee. Please try again later.", ex);
+            }
+        }
+
+
+    }
+}
